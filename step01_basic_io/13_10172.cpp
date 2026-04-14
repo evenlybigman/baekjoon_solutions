58644100ef8975d3f45c372a0c677154c601bf6b
Eve (Juhyeok Kim) <juhyeok.works@gmail.com>
2026-04-15 01:48:07 +0900

    13_10172 강아지 그리기

---
+++ step01_basic_io/13_10172.cpp
@@ -0,0 +1,11 @@
+﻿#include <iostream>
+using namespace std;
+
+//10x5 강아지 그리기
+int main() {
+	cout << "|\\_/|" << endl;
+	cout << "|q p|   /}" << endl;
+	cout << "( 0 )\"\"\"\\ " << endl;
+	cout << "|\"^\"`    |" << endl;
+	cout << "||_/=\\\\__|" << endl;
+}
