10f784d3e2464fa4a8e208a2a58fd4fb14f701e8
Eve (Juhyeok Kim) <juhyeok.works@gmail.com>
2026-04-15 00:09:19 +0900

    10번 2588번 곱셈 성공

--- 10_2588.cpp
+++ 10_2588.cpp
@@ -1,24 +1,18 @@
 ﻿#include <iostream>
+#include <cstdlib>
 using namespace std;
-
-int digits(int x) { //자리수 리턴
-	int counts = 0;
-	while (x != 0) {
-		x / 10;
-		counts++;
-	}
-	return counts;
-}
 
 int main() {
 	//자리수 n 
 	//a의 자릿수와 b의 자리수 파악
-	//a의 10^0 부터 10 ^n승까지 b의 10^0승부터 곱해서 10^ n까지 곱함 
+	//a를 b의 10^0승부터 곱해서 10^ n까지 곱함 
 	//연산이 끝났으면 각 자리수 마다 더함
-	int a, b, an,bn;
-	cin >> a;
-	cin >> b;
-	for (int i = 0; i < digits(a); i++) {
-		
-	}
+	int x;
+	char y[4];
+	cin >> x;
+	cin >> y;
+	cout << x * (y[2] - '0') << endl;
+	cout << x * (y[1] - '0') << endl;
+	cout << x * (y[0] - '0') << endl;
+	cout << x * atoi(y);
 }
